eeade93c00d76090c27fbae1f82dc0b7c68ed5c5
kirill.d <denisov.k.o@yandex.ru>
2026-07-09 14:33:51 +0000

    [Params] Freicoin chain parameters
    Genesis blocks (mainnet 67756db0...), the perpetual emission schedule,
    deployment activation heights (FINALTX, AUXPOW; finaltx active from
    genesis on regtest), the m_headers_sync_params commitments for every
    chain, DNS seeds, StratumPort and DefaultAuxPowPath per chain,
    base58/bech32 prefixes, assumed chain sizes.

--- src/chainparams.h
+++ src/chainparams.h
@@ -1,10 +1,21 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-present The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Copyright (c) 2011-2024 The Freicoin Developers
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of version 3 of the GNU Affero General Public License as published
+// by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef BITCOIN_CHAINPARAMS_H
-#define BITCOIN_CHAINPARAMS_H
+#ifndef FREICOIN_CHAINPARAMS_H
+#define FREICOIN_CHAINPARAMS_H
 
 #include <kernel/chainparams.h> // IWYU pragma: export
 
@@ -28,4 +39,4 @@ const CChainParams &Params();
  */
 void SelectParams(ChainType chain);
 
-#endif // BITCOIN_CHAINPARAMS_H
+#endif // FREICOIN_CHAINPARAMS_H
